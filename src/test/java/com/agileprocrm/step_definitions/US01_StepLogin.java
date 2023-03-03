1ec209dd9cfc16d6fd9919d82fa51ab16abda9eb
Zaferyal <szaferyalcin@gmail.com>
2023-03-03 15:18:11 -0500

    login page updated

--- src/test/java/com/agileprocrm/step_definitions/US01_StepLogin.java
+++ src/test/java/com/agileprocrm/step_definitions/US01_StepLogin.java
@@ -4,28 +4,30 @@ import com.agileprocrm.pages.LoginPage;
 import com.agileprocrm.utilities.ConfigurationReader;
 import com.agileprocrm.utilities.Driver;
 import io.cucumber.java.en.*;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.Assert;
 
 public class US01_StepLogin {
 
-    @Given("User is on the login page of the NextBaseCRM")
+    @Given("User is on the login page")
     public void user_is_on_the_login_page_of_the_next_base_crm() {
         Driver.getDriver().get(ConfigurationReader.getProperty("nextbasecrm.url"));
     }
     LoginPage loginPage = new LoginPage();
 
-    @When("Users enter valid emails {string} and passwords {string}")
-    public void usersEnterValidEmailsAndPasswords(String email, String password) {
+
+    @When("user use username {string} and passcode {string}")
+    public void userUseUsernameAndPasscode(String email, String password) {
         loginPage.inputEmail.sendKeys(email);
         loginPage.inputPassword.sendKeys(password);
     }
-    @When("Clicks the Log In button")
-    public void clicks_the_log_in_button() {
+    @And("user click the login button")
+    public void userClickTheLoginButton() {
         loginPage.btn_log_in.click();
-
     }
 
-        @Then("the users log in successfully and launch the homepage")
+
+        @Then("verify the user should be at the home page")
     public void theUsersLogInSuccesfullyAndLaunchTheHomepage() {
         Assert.assertTrue(Driver.getDriver().getTitle().contains("Portal"));
     }
@@ -41,6 +43,9 @@ public class US01_StepLogin {
     }
 
 
-
+    @And("Clicks the Log In button")
+    public void clicksTheLogInButton() {
+        loginPage.btn_log_in.click();
+    }
 }
 
