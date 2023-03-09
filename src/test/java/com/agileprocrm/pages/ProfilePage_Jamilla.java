0209fa494b970ca9d5bef838c867168a66c4e5d8
Jamilla <casonjamilla@gmail.com>
2023-03-09 17:21:51 -0500

    added feature profile page added step profile page

--- src/test/java/com/agileprocrm/pages/ProfilePage_Jamilla.java
+++ src/test/java/com/agileprocrm/pages/ProfilePage_Jamilla.java
@@ -1,4 +1,16 @@
 package com.agileprocrm.pages;
 
+import com.agileprocrm.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class ProfilePage_Jamilla {
+
+    public ProfilePage_Jamilla() {
+
+
+        PageFactory.initElements(Driver.getDriver(), this);
+
+
+
+    }
 }
